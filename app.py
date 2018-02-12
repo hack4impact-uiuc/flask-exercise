9ee74576440ca936d12ad3fe12f6b2a95aaa9eed
Varun Munjeti <vrunjeti@gmail.com>
2018-02-12 02:44:14 -0600

    create_response wrapper and cleaned up code

--- app.py
+++ app.py
@@ -1,22 +1,47 @@
-from flask import Flask
-from flask import render_template
-from flask import jsonify, request
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
+
+def create_response(data={}, status=200, message=''):
+    """
+    Wraps response in a consistent format throughout the API
+    Format inspired by https://medium.com/@shazow/how-i-design-json-api-responses-71900f00f2db
+    Modifications included:
+    - make success a boolean since there's only 2 values
+    - make message a single string since we will only use one message per response
+
+    IMPORTANT: data must be a dictionary where:
+    - the key is the name of the type of data
+    - the value is the data itself
+    """
+    response = {
+        'success': 200 <= status < 300,
+        'code': status,
+        'message': message,
+        'result': data
+    }
+    return jsonify(response), status
+
+"""
+~~~~~~~~~~~~ API ~~~~~~~~~~~~
+"""
 
 @app.route('/')
 def my_first_route():
-	return "<h1> Hello World! </h1>"
+    return create_response(data='hello world!')
 
-@app.route('/route/<name>')
+@app.route('/mirror/<name>')
 def my_second_route(name):
-     return name
+    return create_response(name)
 
-@app.route('/get_users', methods=['GET'])
+@app.route('/users', methods=['GET'])
 def get_all_users():
     if request.method == 'GET':
-	    return jsonify({'status': 'success', 'data':  ['aria', 'tim', 'varun', 'alex']})
-    else:
-        return jsonify({"status": "failed"})
-        
+        data = { 'users': ['aria', 'tim', 'varun', 'alex'] }
+        return create_response(data)
+
+"""
+~~~~~~~~~~~~ END API ~~~~~~~~~~~~
+"""
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
